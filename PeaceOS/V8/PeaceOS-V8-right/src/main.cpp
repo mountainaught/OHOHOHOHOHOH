7399f0995f5f017d11f43c0a1313082f5dc908fa
Norwegian Bagel <87366642+norwegianbagel@users.noreply.github.com>
2022-03-17 13:34:55 +0300

    Version 8 Upload
    Final version before competition,
    
    TerminOS
    - V8 LEFT
    - V8 RIGHT
    
    PeaceOS
    - V8 LEFT
    - V8 RIGHT

---
+++ PeaceOS/V8/PeaceOS-V8-right/src/main.cpp
@@ -0,0 +1,80 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\rexilius  C:\Users\bagel                         */
+/*    Created:      Fri Dec 17 2021                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+/**
+
+********** BUTTON-PORT ASSIGNMENT **********
+CLAWMOTOR   PORT 10   BUTTONS LEFT-RIGHT
+CLAWLIFT    PORT 9    BUTTONS UP-DOWN
+
+**/
+
+#include "vex.h"
+
+using namespace vex;
+competition Competition;
+
+using namespace robotty;
+
+void paramConfig() {
+  intakeMotor.setVelocity(intakeSpeed, percent);
+  liftMotor.setVelocity(liftSpeed, percent);
+
+  clawElev.setBrake(hold);
+  clawMotor.setBrake(hold);
+
+}
+
+void autonomous() {
+  autoCode();
+}
+
+void usercontrol() {
+  // Consider implementing two-way switch, where entering debug is achieved by way of the main while loop ending
+  // As main() repeats infinitely, usercontrol() is called infinitely
+  // Would be dangerous if it were to start during a match, however, so need easy functionality to disable prior to tournament
+
+  while (1) {
+    Controller1.ButtonL1.pressed(intake::gameIn);
+    Controller1.ButtonL2.pressed(intake::gameOut);
+
+    Controller1.ButtonR1.pressed(mogo::mogoUp); 
+    Controller1.ButtonR2.pressed(mogo::mogoDown);
+
+    Controller1.ButtonB.pressed(powertrain::driveFlip);
+
+    peaceOS::display();
+
+    Controller1.ButtonA.pressed(peaceOS::incrementScreen);
+    Controller1.ButtonX.pressed(peaceOS::main);
+
+    // for terminus robot
+
+    // Call same function following implementation of functions
+    Controller1.ButtonUp.pressed(claw::clawLift);
+    Controller1.ButtonDown.pressed(claw::clawLift);
+
+    Controller1.ButtonRight.pressed(claw::clawMove);
+    Controller1.ButtonLeft.pressed(claw::clawMove);
+
+    wait(20, msec);
+  }
+  
+}
+
+int main() {
+  vexcodeInit();
+
+  paramConfig();
+
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  wait(100, msec);
+}
---
+++ TerminOS/V8/TerminOS-v8-right/src/motors/mogo.cpp
@@ -0,0 +1,54 @@
+#include "vex.h"
+
+using namespace vex;
+using namespace robotty;
+
+
+//new bumper code -fixed March 2022
+
+void mogo::mogoUp(){
+  while (Controller1.ButtonR1.pressing()&& !mogoStop.pressing()){
+          mogoMotor.setVelocity(90.0, percent);
+    mogoMotor.spin(forward);
+  wait(10, msec);
+  }
+ mogoMotor.stop();
+}
+
+void mogo::mogoDown(){
+  while(Controller1.ButtonR2.pressing()){
+          mogoMotor.setVelocity(90.0, percent);
+    mogoMotor.spin(reverse);
+  wait(10, msec);
+  }
+  mogoMotor.stop();
+}
+
+/*
+void mogo::mogoUp(){
+  while(Controller1.ButtonR1.pressing()){
+    if(!mogoStop.pressing()){
+      mogoMotor.spin(forward);
+      wait(10, msec);
+    }
+    mogoMotor.stop();
+  }
+}
+
+  void mogo::mogoDown(){
+    while(Controller1.ButtonR1.pressing()){
+      mogoMotor.spin(reverse);
+    }
+    mogoMotor.stop();
+}
+*/
+// Drive code
+/*
+void mogo::mogo() {
+  while(function::trueYetFalse(Controller1.ButtonR1.pressing(), Controller1.ButtonR2.pressing()) && !mogoStop.pressing()) {
+    mogoMotor.spin(function::driveDir(Controller1.ButtonR1.pressing(), Controller1.ButtonR2.pressing(), forward, reverse), mogoSpeed, percent);
+  wait(10, msec);
+  }
+  mogoMotor.stop();
+}
+*/
